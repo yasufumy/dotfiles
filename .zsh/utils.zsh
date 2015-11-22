1763a777aaeebcab99aa0e00684edf5d4ba2fa11
yasufumi <yasufumi.taniguchi@gmail.com>
2015-11-22 15:51:39 +0900

    add utils.zsh

---
+++ .zsh/utils.zsh
@@ -0,0 +1,40 @@
+# has_command returns true if $1 as a shell command exists
+has.command() {
+    (( $+commands[${1:?too few argument}] ))
+    return $status
+}
+
+# has_command returns true if $1 as a shell function exists
+has.function() {
+    (( $+functions[${1:?too few argument}] ))
+    return $status
+}
+
+# has_command returns true if $1 as a builtin command exists
+has.builtin() {
+    (( $+builtins[${1:?too few argument}] ))
+    return $status
+}
+
+# has_command returns true if $1 as an alias exists
+has.alias() {
+    (( $+aliases[${1:?too few argument}] ))
+    return $status
+}
+
+# has_command returns true if $1 as an alias exists
+has.galias() {
+    (( $+galiases[${1:?too few argument}] ))
+    return $status
+}
+
+# has returns true if $1 exists
+has() {
+    has.function "$1" || \
+        has.command "$1" || \
+        has.builtin "$1" || \
+        has.alias "$1" || \
+        has.galias "$1"
+
+    return $status
+}
