b803990490f513aa9cb7e786897d8c473312f85f
yasufumi <yasufumi.taniguchi@gmail.com>
2015-11-22 15:24:12 +0900

    add aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,21 @@
+# default command
+alias ls="ls -aG"
+alias rm="rm -iv"
+alias mv="mv -iv"
+alias cp="cp -iv"
+alias purge="sudo purge"
+alias grep="grep --color"
+
+# git
+alias gst="git status"
+alias gad="git add --all"
+alias gci="git commit -a --allow-empty-message -m ''"
+alias gph="git push"
+alias gpl="git pull"
+alias gcim="git commit -m"
+alias gbr="git branch"
+alias gco="git checkout"
+alias glg="git log --graph"
+
+# tmux
+alias tmux="tmux -u"
