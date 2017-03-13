1f80988d18c9438224a66f60da15eb3dab316b07
yasufumi <yasufumi.taniguchi@gmail.com>
2017-03-13 16:53:20 +0900

    add zsh-suggestions plugin

--- .zsh/packages.zsh
+++ .zsh/packages.zsh
@@ -14,5 +14,7 @@ zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-history-substring-search"
 
+zplug "zsh-users/zsh-autosuggestions"
+
 zplug "zsh-users/zsh-syntax-highlighting",  \
     defer:2
