bc74d23805977a0a2a0d2cf2d54985fe66f2d232
yasufumi <yasufumi.taniguchi@gmail.com>
2017-11-06 15:28:45 +0900

    remove code for setting up pyenv-virtualenv

--- .zsh/001_utils.zsh
+++ .zsh/001_utils.zsh
@@ -4,11 +4,7 @@ if [[ -x "${HOME}/.pyenv/bin/pyenv" ]]; then
     export PYENV_ROOT="${HOME}/.pyenv"
     path=(${PYENV_ROOT}/bin(N-/^W) ${path})
     eval "$(pyenv init - --no-rehash)"
-    if [[ -x "${HOME}/.pyenv/plugins/pyenv-virtualenv/bin/pyenv-virtualenv" ]]; then
-        eval "$(pyenv virtualenv-init - --no-rehash)"
-    fi
 elif (( $+commands[pyenv] )); then
     # For Homebrew installed pyenv.
     eval "$(pyenv init - --no-rehash)"
-    eval "$(pyenv virtualenv-init - --no-rehash)"
 fi
