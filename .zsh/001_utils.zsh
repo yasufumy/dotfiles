f2b69244f05e37c40b3ccbb0c144a64d986859d3
yasufumi <yasufumi.taniguchi@gmail.com>
2017-12-30 21:15:58 +0900

    update 001_utils.zsh

--- .zsh/001_utils.zsh
+++ .zsh/001_utils.zsh
@@ -1,10 +1,11 @@
 # setup pyenv
 if [[ -x "${HOME}/.pyenv/bin/pyenv" ]]; then
     # For git-cloned pyenv.
-    export PYENV_ROOT="${HOME}/.pyenv"
-    path=(${PYENV_ROOT}/bin(N-/^W) ${path})
+    path=(${HOME}/.pyenv/bin(N-/^W) ${path})
+    export PYENV_ROOT=$(pyenv root)
     eval "$(pyenv init - --no-rehash)"
 elif (( $+commands[pyenv] )); then
     # For Homebrew installed pyenv.
+    export PYENV_ROOT=$(pyenv root)
     eval "$(pyenv init - --no-rehash)"
 fi
